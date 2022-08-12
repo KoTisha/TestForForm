47f83213a6b637835a0cecb23cec631614901882
Konstantin Tishkov <kokotrew@gmail.com>
2022-08-12 19:01:10 +0500

    Added views

--- testforform/core/urls.py
+++ testforform/core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
-    # path('',)
+    path('', RedirectView.as_view(url='/login')),
+    path('/feedback', views.FeedbackWriteView.as_view(), name='feedback_write_page'),
+    path('/feedback_list', views.FeedbackListView.as_view(), name='feedback_list_page'),
+    path('/login', views.LoginView.as_view(), name='login_page'),
 ]
--- testforform/core/views.py
+++ testforform/core/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
+from .models import Feedback
 
 # Create your views here.
+
+class LoginView(FormView):
+    pass
+
+class FeedbackWriteView(FormView):
+    pass
+
+class FeedbackListView(ListView):
+    pass
+
+
 
